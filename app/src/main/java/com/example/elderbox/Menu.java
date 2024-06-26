e5cf30874eec1bf5935a3f088ef02d3227e010a1
King <230032762@stu.vtc.edu.hk>
2024-06-26 16:55:52 +0800

    update version 2.1_nav

--- app/src/main/java/com/example/elderbox/Menu.java
+++ app/src/main/java/com/example/elderbox/Menu.java
@@ -1,20 +1,33 @@
 package com.example.elderbox;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.elderbox.ranking.Ranking;
 import com.example.elderbox.record.Record;
 
+
 public class Menu extends AppCompatActivity {
 
-    Button login, setting;
-    Button game,record,ranking,logout;
+    Button login, setting, game, record, ranking, exit;
+
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, settings, share, about, logout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +41,18 @@ public class Menu extends AppCompatActivity {
         game = findViewById(R.id.btn4);
         record = findViewById(R.id.btn5);
         ranking = findViewById(R.id.btn6);
-        logout = findViewById(R.id.btn7);
+        exit = findViewById(R.id.btn7);
+
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        settings = findViewById(R.id.settings);
+        share = findViewById(R.id.share);
+        about = findViewById(R.id.about);
+        logout= findViewById(R.id.logout);
+
 
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -71,12 +95,59 @@ public class Menu extends AppCompatActivity {
             }
         });
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finishAffinity(); // end all
             }
         });
+
+
+
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openDrawer(drawerLayout);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Menu.this, SettingsActivity.class);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Menu.this, ShareActivity.class);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Menu.this, AboutActivity.class);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu.this,"Logout",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
     public void back(View view) {
@@ -84,4 +155,32 @@ public class Menu extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+
+
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/elderbox/ShareActivity.java
@@ -0,0 +1,99 @@
+package com.example.elderbox;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class ShareActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, settings, share, about, logout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        settings = findViewById(R.id.settings);
+        share = findViewById(R.id.share);
+        about = findViewById(R.id.about);
+        logout= findViewById(R.id.logout);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDrawer(drawerLayout);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ShareActivity.this, Menu.class);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ShareActivity.this, SettingsActivity.class);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(ShareActivity.this, AboutActivity.class);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ShareActivity.this,"Logout",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}
